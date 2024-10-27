aff16a600cbb13010b4132f0b72d8b2e4218060a
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2024-10-27 05:11:36 +0000

    working!

--- api/static/script.js
+++ api/static/script.js
@@ -1,11 +1,17 @@
-const wordList = [
-    'dead', 'death', 'decay', 'december', 'decline', 'deep',
-    'degree', 'delay', 'deliver', 'demand', 'democracy',
-    'demonstrate', 'dental', 'deny', 'depend', 'deposit',
-    'depression', 'depth', 'deputy', 'derive', 'describe',
-    'desert', 'design', 'desire', 'desk', 'destroy',
-    'detail', 'detect', 'determine', 'develop'
-];
+const wordList = bible_books = [
+       "Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua",
+       "Judges", "Ruth", "1 Samuel", "2 Samuel", "1 Kings", "2 Kings",
+       "1 Chronicles", "2 Chronicles", "Ezra", "Nehemiah", "Esther", "Job",
+       "Psalm", "Proverbs", "Ecclesiastes", "Song of Solomon", "Isaiah",
+       "Jeremiah", "Lamentations", "Ezekiel", "Daniel", "Hosea", "Joel",
+       "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk",
+       "Zephaniah", "Haggai", "Zechariah", "Malachi",
+       "Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1 Corinthians",
+       "2 Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians",
+       "1 Thessalonians", "2 Thessalonians", "1 Timothy", "2 Timothy", "Titus",
+       "Philemon", "Hebrews", "James", "1 Peter", "2 Peter", "1 John", "2 John",
+       "3 John", "Jude", "Revelation"
+   ];
 
 const textInput = document.getElementById('textInput');
 const suggestionsDiv = document.getElementById('suggestions');
