90d8eacb0ccdf0e2395be11ef05df3396cdcc0e7
Kendrick <137743645+pufferfish3e@users.noreply.github.com>
2024-10-28 22:26:31 +0800

    Update flask_app.py

--- api/flask_app.py
+++ api/flask_app.py
@@ -1,4 +1,5 @@
 import logic
+import os
 from flask import Flask, render_template
 from markupsafe import Markup
 
@@ -13,4 +14,5 @@ def home():
     return render_template('index.html', text_content=Markup(data))  
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host='0.0.0.0', port=port)
